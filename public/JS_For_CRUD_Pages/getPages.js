e05e03129154f6f2d25cb104dc1b6f8001662bfd
Leta Asefa <letaassefa7@gmail.com>
2023-12-23 09:15:26 +0300

    confirmation text is added for getPages.html

--- public/JS_For_CRUD_Pages/getPages.js
+++ public/JS_For_CRUD_Pages/getPages.js
@@ -8,12 +8,20 @@ let response = document.getElementById('response');
 getButton.onclick = function () {
     
     id = searchBar.value 
+    response.innerHTML = ""
 
     fetch("http://localhost:8000/api/getHome/" + id)
         .then((res) => res.text())
         .then((html) => {
+            if (html == "Invalid Id") {
+                response.innerHTML = "Invalid Id"
+                response.classList.add('text-danger')
+                return
+            }
             const newTab = window.open();
             newTab.document.write(html);
+            response.innerHTML = "Opened in the next tab !"
+            response.classList.replace('text-danger','text-info')
         
         })
         .catch(error => {
